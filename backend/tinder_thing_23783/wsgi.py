7ec5cb327dc7b4ad334bd266b754e94fb57028b4
Crowdbotics <team@crowdbotics.com>
2021-01-12 07:55:53 +0000

    Update models

--- backend/tinder_thing_23783/wsgi.py
+++ backend/tinder_thing_23783/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinder_thing_23783.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tinder_thing_23783.settings")
 
 application = get_wsgi_application()
